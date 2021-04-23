36df5fc3eca493e41518761b6a41ec4045501234
makarenk0 <45842815+makarenk0@users.noreply.github.com>
2021-04-23 20:49:02 +0300

    tranfered from own ws to Fleck library

--- WebsocketAdapter/MainWebsocketListener.cs
+++ WebsocketAdapter/MainWebsocketListener.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Fleck;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -18,42 +20,17 @@ namespace WebsocketAdapter
 
         public MainWebsocketListener(int port, string ipAddress)
         {
-            _port = port;
-            _ipAddress = ipAddress;
+           
 
-            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(_ipAddress), _port);
-            _listener = new TcpListener(IPAddress.Parse(_ipAddress), _port);//new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            _listener.Start();
-
-            //int minWorker, minIOC;
-            //ThreadPool.GetMinThreads(out minWorker, out minIOC);
-            //ThreadPool.SetMinThreads(DEFAULT_PARALLEL_THREADS_NUM, minIOC);
-            //Console.WriteLine(String.Concat("Resetting default thread pool volume from ", minWorker, " to ", DEFAULT_PARALLEL_THREADS_NUM));
-
-            Listen();
-        }
-
-        private void Listen()
-        {
-            try
+            //X509Certificate2 certificate = new X509Certificate2("C:\\test.com.pfx", "14032001");
+            
+            var server = new WebSocketServer($"ws://{ipAddress}");
+            //server.Certificate = certificate;
+            server.Start(socket =>
             {
-                int clientsAmount = 0;
-                while (true)
-                {
-                    Console.WriteLine("Waiting for web clients...");
-                    //Socket client = _listener.Accept();
-                    TcpClient client = _listener.AcceptTcpClient();
-                    WebsocketClientObject clientObject = new WebsocketClientObject(client);
-
-                    ThreadPool.QueueUserWorkItem(clientObject.Process);
-
-                    ++clientsAmount;
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+                Console.WriteLine("New user");
+                WebsocketClientObject clientObject = new WebsocketClientObject(socket);
+            });
         }
     }
 }
